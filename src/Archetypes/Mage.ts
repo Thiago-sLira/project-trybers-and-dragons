7395039205629ada198500a8ac7ce2edd0d09a28
Thiago Lira <thiago.slira@outlook.com>
2023-05-05 18:50:47 -0300

    Classe mage construída com seus devidos atributos e métodos

--- src/Archetypes/Mage.ts
+++ src/Archetypes/Mage.ts
@@ -1,5 +1,25 @@
+import { EnergyType } from '../Energy';
 import Archetype from './Archetype';
 
 export default class Mage extends Archetype {
+  private _energyType: EnergyType;
+  private static _magesInstancesCreated = 0;
 
+  constructor(
+    name: string,
+    special: number,
+    cost: number,
+  ) {
+    super(name, special, cost);
+    this._energyType = 'mana';
+    Mage._magesInstancesCreated += 1;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    return Mage._magesInstancesCreated;
+  }
 }
