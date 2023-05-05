240368c5a3af6c409f6ab12f6f02741847582cbb
Thiago Lira <thiago.slira@outlook.com>
2023-05-05 18:57:32 -0300

    Classe Warrior construída com seus devidos atributos e métodos

--- src/Archetypes/Warrior.ts
+++ src/Archetypes/Warrior.ts
@@ -1,5 +1,25 @@
+import { EnergyType } from '../Energy';
 import Archetype from './Archetype';
 
 export default class Warrior extends Archetype {
+  private _energyType: EnergyType;
+  private static _warriorInstancesCreated = 0;
 
+  constructor(
+    name: string,
+    special: number,
+    cost: number,
+  ) {
+    super(name, special, cost);
+    this._energyType = 'stamina';
+    Warrior._warriorInstancesCreated += 1;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    return Warrior._warriorInstancesCreated;
+  }
 }
