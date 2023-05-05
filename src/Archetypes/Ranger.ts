6400e4e83e0e4d43efdbea546500ff4c32e30a57
Thiago Lira <thiago.slira@outlook.com>
2023-05-05 18:56:58 -0300

    Classe Ranger construída com seus devidos atributos e métodos

--- src/Archetypes/Ranger.ts
+++ src/Archetypes/Ranger.ts
@@ -1,5 +1,25 @@
+import { EnergyType } from '../Energy';
 import Archetype from './Archetype';
 
 export default class Ranger extends Archetype {
+  private _energyType: EnergyType;
+  private static _rangerInstancesCreated = 0;
 
+  constructor(
+    name: string,
+    special: number,
+    cost: number,
+  ) {
+    super(name, special, cost);
+    this._energyType = 'stamina';
+    Ranger._rangerInstancesCreated += 1;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    return Ranger._rangerInstancesCreated;
+  }
 }
