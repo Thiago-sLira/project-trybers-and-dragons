96e505b6c057a734a9c0b008e2f37061de1f2e25
Thiago Lira <thiago.slira@outlook.com>
2023-05-08 15:43:37 -0300

    Método fight sobrescrito e modo de batalha on!!

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,16 +1,59 @@
 import Character from '../Character';
+import Fighter from '../Fighter';
 import Battle from './Battle';
 
 export default class PVP extends Battle {
-  private _characterRed: Character;
-  private _characterBlue: Character;
+  private _characterRed: Fighter;
+  private _characterBlue: Fighter;
 
   constructor(
-    characterRed: Character,
-    characterBlue: Character,
+    characterRed: Fighter,
+    characterBlue: Fighter = new Character('Default'),
   ) {
     super(characterRed);
     this._characterRed = characterRed;
     this._characterBlue = characterBlue;
   }
+
+  redAttacks(): void {
+    if (this._characterRed.lifePoints > 0) {
+      this._characterRed.attack(this._characterBlue);
+    }
+  }
+
+  blueAttacks(): void {
+    if (this._characterBlue.lifePoints > 0) {
+      this._characterBlue.attack(this._characterRed);
+    }
+  }
+
+  verifyBattle(): boolean {
+    return this._characterBlue.lifePoints > 0
+      && this._characterRed.lifePoints > 0;
+  }
+
+  fight(): number {
+    while (this.verifyBattle()) {
+      // if (this._characterBlue.dexterity > this._characterRed.dexterity) {
+      this.blueAttacks();
+      this.redAttacks();
+      // } else {
+      //   this.redAttacks();
+      //   this.blueAttacks();
+      // }
+    }
+    return super.fight();
+  }
 }
+
+// const characterBlue = new Character('blue');
+// const characterRed = new Character('red');
+// console.log('LIFEPOINTS', characterRed.lifePoints);
+
+// const pvp = new PVP(characterRed, characterBlue);
+
+// for (let index = 0; index < 20; index += 1) {
+//   characterRed.levelUp();
+// }
+
+// console.log(pvp.fight());
