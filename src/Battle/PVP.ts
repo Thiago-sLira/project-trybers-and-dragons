2ec27883fdcf3406a552c4a3a3d625fd01e516a8
Thiago Lira <thiago.slira@outlook.com>
2023-05-08 14:35:38 -0300

    Implementado atritubos de PVP

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,5 +1,16 @@
+import Character from '../Character';
 import Battle from './Battle';
 
 export default class PVP extends Battle {
-  
+  private _characterRed: Character;
+  private _characterBlue: Character;
+
+  constructor(
+    characterRed: Character,
+    characterBlue: Character,
+  ) {
+    super(characterRed);
+    this._characterRed = characterRed;
+    this._characterBlue = characterBlue;
+  }
 }
