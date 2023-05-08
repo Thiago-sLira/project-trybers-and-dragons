d85952ae7672d716662f3d8d8c9de9eeef10a12c
Thiago Lira <thiago.slira@outlook.com>
2023-05-08 16:59:03 -0300

    Implementando método fight

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -14,4 +14,36 @@ export default class PVE extends Battle {
     this._character = character;
     this._monsters = monsters;
   }
+
+  characterAttacks(monster: SimpleFighter): void {
+    if (this._character.lifePoints > 0) {
+      this._character.attack(monster);
+    }
+  }
+
+  monsterAttacks(monster: SimpleFighter): void {
+    if (monster.lifePoints > 0) {
+      monster.attack(this._character);
+    }
+  }
+
+  verifyCharacter(): boolean {
+    return this._character.lifePoints > 0;
+  }
+
+  // verifyMonster(monster: SimpleFighter): boolean {
+  //   return monster.lifePoints > 0;
+  // }
+
+  fight(): number {
+    while (this.verifyCharacter()) {
+      this._monsters.forEach((monster) => {
+        while (monster.lifePoints > 0) {
+          this._character.attack(monster);
+          monster.attack(this._character);
+        }
+      });
+    }
+    return super.fight();
+  }
 }
